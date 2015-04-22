5d924142a12f6c50932c38bb62966c8f62d992ff
Bob Bloom <info@southlasalle.com>
2015-04-22 11:23:22 -0400

    Modify main seeder for new CRM API package. #27

--- plumbing/database/seeds/DatabaseSeeder.php
+++ plumbing/database/seeds/DatabaseSeeder.php
@@ -3,9 +3,31 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use \Illuminate\Contracts\Foundation\Application;
+use \Illuminate\Support\Facades\File;
+
 class DatabaseSeeder extends Seeder {
 
-	/**
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $application;
+
+    /**
+     * Inject the application instance
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+
+    /**
 	 * Run the database seeds.
 	 *
 	 * @return void
@@ -14,8 +36,16 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
+        // UserManagement & CMS API packages are mandatory
 		$this->call('UsermanagementTableSeeder');
         $this->call('LasallecmsapiTableSeeder');
+        
+
+        // LaSalleCRM is optional
+        if ( File::exists($this->application->databasePath() . '/seeds/LasallecrmapiTableSeeder.php') )
+        {
+            $this->call('LasallecrmapiTableSeeder');
+        }
 	}
 
 }
