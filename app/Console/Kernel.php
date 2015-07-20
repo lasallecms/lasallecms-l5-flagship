8bc03d4b9cab7ddd498c30864c033b1ce70279c9
Bob Bloom <info@southlasalle.com>
2015-07-20 09:31:17 -0400

    Add scheduler for sitemap and backup packages. #36, #37

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,39 @@
 
 namespace App\Console;
 
+/**
+ *
+ * LaSalle Content Management System, based on the Laravel 5 Framework
+ * Copyright (C) 2015  The South LaSalle Trading Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @package    LaSalle Content Management System
+ * @version    1.0.0
+ * @link       http://LaSalleCMS.com
+ * @copyright  (c) 2015, The South LaSalle Trading Corporation
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ * @author     The South LaSalle Trading Corporation
+ * @email      info@southlasalle.com
+ *
+ */
+
+
+// http://laravel.com/docs/5.1/scheduling
+
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +46,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\Inspire::class,
+        \Spatie\Backup\Commands\BackupCommand::class,
     ];
 
     /**
@@ -24,7 +57,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        $schedule->command('BackupCommand')
+                 ->dailyAt('04:00')
+                 ->emailOutputTo('foo@example.com')
+        ;
+
+        $schedule->call(function () {
+            return Redirect::route('sitemap');
+        })->dailyAt('04:30');
     }
 }
